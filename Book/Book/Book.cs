826a86c1fe659649ed29dbfe5a18b73f19c21f92
Janne Liimatta <janneliimatta@hotmail.com>
2018-01-18 09:47:27 +0200

    Bookki valmis

---
+++ Book/Book/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Book
+    {
+        public string name;
+        public string author;
+        public int id;
+        public int price;
+
+        public Book(string Name = "no name", string Author = "no author", int Id = 0, int Price = 0)
+        {
+            name = Name;
+            author = Author;
+            id = Id;
+            price = Price;
+
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.price > book.price)
+                return $"{this.name} on kalliimpi kuin {book.name} kirja";
+            else
+                return $"{book.name} on kalliimpi kuin {this.name} kirja";
+        }
+        public void Askdata()
+        {
+            Console.WriteLine("Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Author:");
+            author = Console.ReadLine();
+            Console.WriteLine("id:");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price");
+            price = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
--- Book/Book/Program.cs
+++ Book/Book/Program.cs
@@ -10,41 +10,22 @@ namespace Book
     {
         static void Main(string[] args)
         {
-            Book aapinen = new Book();
-            aapinen.name = "abc-kirja";
-            aapinen.author = "jeesus";
-            aapinen.id = 1;
-            aapinen.price = 20;
 
-            Console.WriteLine("{0} \n author: {1} \n id: {2} \n price: {3}",aapinen.name, aapinen.author, aapinen.id, aapinen.price);
+            Console.WriteLine("Give book info");
+
+            Book newBook= new Book();
+            newBook.Askdata();
+
+            Book book2 = new Book();
+            book2.Askdata();
+
+
+            Console.WriteLine(newBook.CompareBook(book2));
             Console.ReadLine();
 
 
     
         }
     }
-    class Book
-    {
-        public string name;
-        public string author;
-        public int id;
-        public int price;
-
-        public Book(string Name = "no name", string Author = "no author", int Id = 0, int Price = 0)
-        {
-            name = Name;
-            author = Author;
-            id = Id;
-            price = Price;
-            
-        }
-
-        public string CompareBook(Book book)
-        {
-            if (this.price > book.price)
-                return "{this.Title} on kalliimpi kuin {book.Title} kirja)";
-        }
-            
-
-    }
+    
 }
