ded4b6c93b16be63483ea964229130777ca0fe14
Janne Liimatta <janneliimatta@hotmail.com>
2018-02-22 10:04:17 +0200

    Kirjallisuustehtäviä

---
+++ Book_Author/Book_Author/Book.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Author
+{
+    class Book
+    {
+        public string Name;
+        public readonly string Author;
+
+        public string Publisher;
+        public double Price;
+        public static string ThemeName;
+        public double Pprice
+        {
+            get
+            {
+                return Price;
+            }
+            set
+            {
+                if (value > 30)
+                {
+                    Price = value * 0.9;
+                }
+                else
+                    Price = value;
+            }
+        }
+        
+
+        public Book(string name, string author, string publisher, double price)
+        {
+            Name = name;
+            Author = author;
+            Publisher = publisher;
+            Price = price;
+        }
+
+        public void ChangeTheme()
+        {
+            Console.WriteLine("Anna aapiselle uusi teema");
+            string newTheme = Console.ReadLine();
+            ThemeName = newTheme;
+            Console.WriteLine($"Aapisen uusi teema on: {ThemeName}");
+        }
+
+        public void GetData()
+        {
+            Console.WriteLine("Anna kirjan nimi");
+            Name = Console.ReadLine();
+            Console.WriteLine("Anna julkaisijan nimi");
+            Publisher = Console.ReadLine();
+            Console.WriteLine("Anna kirjan hinta");
+            Price = Convert.ToDouble(Console.ReadLine());
+        }
+        public string search()
+        {
+            string input = Console.ReadLine();
+            return input;
+            
+        }
+
+        public bool GetBook(string Search)
+        {
+            return (this.Name == Search);
+            //string Search = search();
+            //if (this.Name == Search)
+            //{
+            //    Console.WriteLine($"Kirjan tiedot");
+            //    // {books[i].Name} {books[i].Author} {books[i].Publisher} {books[i].Price}
+               
+            //}
+            //else
+            //    Console.WriteLine("Kirjaa ei löydy");
+        }
+
+    }
+}
---
+++ Book_Author/Book_Author/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Author
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[2];
+            books[0] = new Book("aapinen", "jesse", "otava", 100);
+
+            books[1] = new Book("Osakesäästämisen abc", "Janne", "otava", 30);
+
+
+            Console.WriteLine("Anna kirjan nimi");
+
+            
+            int i = 0;
+            string Search = Console.ReadLine();
+            do
+            {
+                if (books[i].GetBook(Search))
+                {
+                    Console.WriteLine($"Title: {books[i].Name} \nAuthor: {books[i].Author}  \nPrice: {books[i].Price}  \nPublisher: {books[i].Publisher}");
+                    break;
+                }
+                else
+                    i++;
+            }
+            while (i < 2);
+            if (i == 2)
+                Console.WriteLine("Kirjaa ei löytynyt");
+
+            books[0].ChangeTheme();
+            Console.WriteLine("Anna Aapiselle uusi hinta");
+            books[0].Pprice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Aapisen hinta on:");
+            System.Console.WriteLine(books[0].Price);
+
+            Console.WriteLine($"Aapisen kirjoittaja on: {books[0].Author}");
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+        
+    
+}
