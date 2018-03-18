a2873de36b6d9729bc6cafb32aa72ca4d113e012
Janne Liimatta <janneliimatta@hotmail.com>
2018-03-18 19:40:44 +0200

    vehicle tehtävä

---
+++ Vehicle/Vehicle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle("audi","audi","2000", 5000);
+            Car audi = new Car();
+            Vehicle vehicle2 = new Car()
+        }
+    }
+}
---
+++ Vehicle/Vehicle/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    using System;
+    class Vehicle
+    {
+        protected string Type;
+        protected string Make;
+        protected string YearModel;
+        protected double Price;
+
+        public Vehicle(string type, string make, string yearModel, double price)
+        {
+            Type = type;
+            Make = make;
+            YearModel = yearModel;
+            Price = price;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Tyyppi: {Type}, /nMerkki: {Make}, /nVuosimalli{YearModel}, /nHinta{Price}");
+        }
+    }
+}
