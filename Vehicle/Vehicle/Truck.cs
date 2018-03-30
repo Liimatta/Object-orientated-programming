9a3b2250f71d9e67aff04a15f621a0910bf62f0e
Janne Liimatta <janneliimatta@hotmail.com>
2018-03-30 11:49:41 +0300

    vehicle

---
+++ Vehicle/Vehicle/Truck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Truck : Car
+    {
+        protected int LoadWeight;
+        protected int MPG;
+
+
+
+        public Truck(int loadWeight, int mPG) : base( Type, Make, Yearmodel, Price, EngineSize, Model, NbrOfDoors)
+        {
+            LoadWeight = loadWeight;
+            MPG = mPG;
+
+        }
+
+    
+
+        
+
+    }
+}
